a61fa1136dd6259ca394ba538ffe3fe145999ad1
Matias Ezequiel Millan <matiasezequielmillan@gmail.com>
2019-05-27 21:57:41 -0300

    Nuevo: añade documentacion XML  a los proyectos Cambios: documentacion de clase datos grupos  datos Usuario campos encapsulados y contructor

--- Datos/DatosGrupo.cs
+++ Datos/DatosGrupo.cs
@@ -5,21 +5,43 @@ using System.Data.SqlClient;
 
 
 namespace Datos
-{
+{  /// <summary>
+/// Esta Clase se utiliza para realizar ejecutar Altas, Bajas , Modificacion, Busquedas y Listados de elementos para la tabla grupos
+/// </summary>
     public class DatosGrupo
     {
+        //Encapsulado de los campos de la tabla
         private string nombreGrupo;
         private int IdNombre;
         private string estadoGrupo;
-        private SqlCommand SqlComando;
-
-        ConneccionSql ConneccionSql;
-
+        
+        /// <summary>
+        /// encapsulamiento del campo Nombre del grupo
+        /// </summary>
         public string NombreGrupo { get => nombreGrupo; set => nombreGrupo = value; }
+        /// <summary>
+        /// Encapsulamiento del campo ID del Grupo
+        /// </summary>
         public int IdNombre1 { get => IdNombre; set => IdNombre = value; }
+        /// <summary>
+        /// encapsulamiento del campo estado del grupo
+        /// </summary>
         public string EstadoGrupo { get => estadoGrupo; set => estadoGrupo = value; }
 
+
+        private SqlCommand SqlComando;
+        ConneccionSql ConneccionSql;
+
+           /// <summary>
+           /// Contructor de la clase DatosGrupos           
+           /// </summary>
         public DatosGrupo() { }
+       /// <summary>
+       /// contructo de la clase datos grupo
+       /// </summary>
+       /// <param name="id"> ID del Grupo</param>
+       /// <param name="nombre"> Nombre del Grupo</param>
+       /// <param name="estado">Estado Del Grupo</param>
         public DatosGrupo(int id, string nombre, string estado)
         {
             NombreGrupo = nombre;
@@ -27,7 +49,11 @@ namespace Datos
             EstadoGrupo = estado;
         }
 
-
+        /// <summary>
+        /// Metodo para ingresar un nuevo grupo de usuarios
+        /// </summary>
+        /// <param name="grupo">ingresar objeto del tipo grupo</param>
+        /// <returns>respuesta Si o No fue exitosa la operacion</returns>
         public string IngresarGrupo(DatosGrupo grupo)
         {
             string rta = "";
@@ -52,9 +78,9 @@ namespace Datos
                     SqlDbType = SqlDbType.VarChar,
                     Size = 120,
                     Value = grupo.NombreGrupo
-                                        
+
                 };
-                
+
                 SqlComando.Parameters.Add(sqlparametersNombre);
 
                 SqlParameter sqlparametreEstado = new SqlParameter
@@ -65,7 +91,7 @@ namespace Datos
                     Value = grupo.EstadoGrupo
                 };
                 SqlComando.Parameters.Add(sqlparametreEstado);
-                
+
 
                 rta = SqlComando.ExecuteNonQuery() == 1 ? "OK" : "No se pudo Ingresar el registro ";
 
@@ -82,8 +108,12 @@ namespace Datos
 
         }
 
-
-       public string UpdateGrupos (DatosGrupo grupo)
+        /// <summary>
+        /// metodo para  modificar un grupo existente
+        /// </summary>
+        /// <param name="grupo">ingresar objeto del tipo grupo</param>
+        /// <returns>respuesta Si o No fue exitosa la operacion</returns>
+        public string UpdateGrupos(DatosGrupo grupo)
         {
 
             string rta = "";
@@ -140,7 +170,11 @@ namespace Datos
         }
 
 
-
+        /// <summary>
+        /// Elimina un grupo existente
+        /// </summary>
+        /// <param name="grupo"></param>
+        /// <returns>respuesta Si o No fue exitosa la operacion</returns>
         public string EliminarGrupo(DatosGrupo grupo)
         {
 
@@ -161,18 +195,18 @@ namespace Datos
                     CommandType = CommandType.StoredProcedure
                 };
 
-                SqlParameter sqlparametersIdnGrupo= new SqlParameter
+                SqlParameter sqlparametersIdnGrupo = new SqlParameter
                 {
                     ParameterName = "@idGrupo",
                     SqlDbType = SqlDbType.Int,
-                    
+
                     Value = grupo.IdNombre1
 
                 };
 
                 SqlComando.Parameters.Add(sqlparametersIdnGrupo);
 
-                
+
 
                 rta = SqlComando.ExecuteNonQuery() == 1 ? "OK" : "No se pudo Ingresar el registro ";
 
@@ -190,7 +224,10 @@ namespace Datos
         }
 
 
-
+              /// <summary>
+              /// lista todos los grupos existentes en el sistema
+              /// </summary>
+              /// <returns>Devuelve una lista de todos los grupos del tipo datatable</returns>
         public DataTable ListarGrupos()
         {
             DataTable dataTable;
@@ -228,7 +265,11 @@ namespace Datos
 
 
         }
-
+         /// <summary>
+         /// buscar por ID un grupo en particular
+         /// </summary>
+         /// <param name="datosGrupo">Ingrese el Id del grupo a encontrar</param>
+         /// <returns>devuelve el grupo </returns>
         public DataTable BuscarGrupos(DatosGrupo datosGrupo)
         {
             DataTable dataTable;
--- Datos/DatosUsuarios.cs
+++ Datos/DatosUsuarios.cs
@@ -1,12 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Acceso_a_Datos;
+using System;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Datos
-{
-    class DatosUsuarios
+{                      /// <summary>
+/// Esta Clase se utiliza para realizar ejecutar Altas, Bajas , Modificacion, Busquedas y Listados de elementos para la tabla Usuarios 
+/// </summary>
+    public class DatosUsuarios
     {
+        
+            private int     DNI  ;
+            private string apellido ;
+            private string nombreusuario      ;
+            private char sexo ;
+            private int edad ;
+        private string email;
+            private int  telefono ;
+            private string  pais;
+            private string provincia ;
+            private string direcion ;
+            private int  codigopostal ;
+            private int estado ;
+
+      
+       
+        public int DNI1 { get => DNI; set => DNI = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public string Nombreusuario { get => nombreusuario; set => nombreusuario = value; }
+        public char Sexo { get => sexo; set => sexo = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public string Email { get => email; set => email = value; }
+        public int Telefono { get => telefono; set => telefono = value; }
+        public string Pais { get => pais; set => pais = value; }
+        public string Provincia { get => provincia; set => provincia = value; }
+        public string Direcion { get => direcion; set => direcion = value; }
+        public int Codigopostal { get => codigopostal; set => codigopostal = value; }
+        public int Estado { get => estado; set => estado = value; }
+
+            
+        public DatosUsuarios()
+        {
+        }
+
+        public DatosUsuarios(int dNI1, string apellido, string nombreusuario, char sexo, int edad, string email, int telefono, string pais, string provincia, string direcion, int codigopostal, int estado)
+        {
+
+            DNI1 = dNI1;
+            Apellido = apellido;
+            Nombreusuario = nombreusuario;
+            Sexo = sexo;
+            Edad = edad;
+            Email = email;
+            Telefono = telefono;
+            Pais = pais;
+            Provincia = provincia;
+            Direcion = direcion;
+            Codigopostal = codigopostal;
+            Estado = estado;
+        }
+
+
+
+
+
     }
 }
